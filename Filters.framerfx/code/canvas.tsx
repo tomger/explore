34d629b889a42c5f1a7aa957403ed884daf1e674
tomg <tom.germeau@gmail.com>
2019-06-23 11:08:57 -0400

    yuck

--- Filters.framerfx/code/canvas.tsx
+++ Filters.framerfx/code/canvas.tsx
@@ -4,7 +4,9 @@ const canvas = CanvasStore.shared(); // CANVAS_DATA;
 
 export const colors = Object.freeze({
     /** #333333 */
-    "gray90": "var(--token-46aa6ae7-aeb1-4482-bbd3-a1aece523c9a, rgb(51, 51, 51))",
+    "gray 90": "var(--token-46aa6ae7-aeb1-4482-bbd3-a1aece523c9a, rgb(51, 51, 51))",
+    /** #7F7F7F */
+    "gray 70": "var(--token-51e62833-889d-41bf-b346-5c037cf4235e, rgb(127, 127, 127))",
     /** #FFFFFF */
     "white": "var(--token-0cf5db7f-6930-45e2-99dd-598d892a34c3, rgb(255, 255, 255))",
 })
