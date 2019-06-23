f07e1393683bfda1b6e58f6512e970142ed29897
tomg <tom.germeau@gmail.com>
2019-06-23 10:38:50 -0400

    leggo

--- Filters.framerfx/code/canvas.tsx
+++ Filters.framerfx/code/canvas.tsx
@@ -1,3 +1,10 @@
 // WARNING: this file is auto generated, any changes will be lost
 import { createDesignComponent, CanvasStore } from "framer"
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
+
+export const colors = Object.freeze({
+    /** #333333 */
+    "gray90": "var(--token-46aa6ae7-aeb1-4482-bbd3-a1aece523c9a, rgb(51, 51, 51))",
+    /** #FFFFFF */
+    "white": "var(--token-0cf5db7f-6930-45e2-99dd-598d892a34c3, rgb(255, 255, 255))",
+})
