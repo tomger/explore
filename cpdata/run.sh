a7ec5f1c7b7b46f4c3fd0bde30d02458530ce3d4
Tom Germeau <tgermeau@classpass.com>
2019-07-29 11:11:11 -0400

    more example

--- cpdata/run.sh
+++ cpdata/run.sh
@@ -4,5 +4,9 @@ convert -crop 750x1000 output/denver.png output/denver_%d.png
 cat output/denver.json |  ./proto_classes.js  > output/denver_classes.js
 
 
+// NJ
+// {"search_request":{"filters":{"vertical":"fitness","date":"2019-07-29","lat":40.72477651911137,"lon":-74.04397508386175,"result_type":"VENUE","tag":[],"map_bounds":"-74.0644952489028,40.69066673529619,-74.02345491884574,40.75886883028423"}}}
 
-./get.js --request '{"search_request":{"filters":{"lat":40.71962920625383,"lon":-74.04500010264353,"result_type":"VENUE","tag":[],"map_bounds":"-74.05774163095468,40.708552917650735,-74.03225857432639,40.730703652111686"}}}' | ./proto_classes.js | npx babel-node tohtml.js | ./booth.js --output output/latest.png
+
+./get.js --request '{"search_request":{"filters":{"date":"2019-06-11","lat":40.71962920625383,"lon":-74.04500010264353,"result_type":"VENUE","tag":[],"map_bounds":"-74.05774163095468,40.708552917650735,-74.03225857432639,40.730703652111686"}}}' | ./proto_classes.js | npx babel-node tohtml.js | ./booth.js --output output/latest.png
+./get.js --request '{"search_request":{"filters":{"date":"2019-07-29","lat":40.72477651911137,"lon":-74.04397508386175,"result_type":"VENUE","tag":[],"map_bounds":"-74.0644952489028,40.69066673529619,-74.02345491884574,40.75886883028423"}}}' | ./proto_venues.js | npx babel-node tohtml.js | ./booth.js --output output/latest.png && convert -crop 750x1000 output/latest.png output/%d.png
