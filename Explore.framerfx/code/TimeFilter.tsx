56f63ca887f3e022acf5b1d89faf6402b8ea2b58
tomg <tom.germeau@gmail.com>
2019-08-17 17:17:20 -0400

    adding pill bar

--- Explore.framerfx/code/TimeFilter.tsx
+++ Explore.framerfx/code/TimeFilter.tsx
@@ -5,33 +5,82 @@ const data = Data({})
 
 export function TimeFilter(props) {
     // var [venueList, setVenueList] = React.useState(data.venueList)
+    /*
+    Time          Popup
+    Sort          Popup (could be single "Top rated")
+    Credits       Popup
+    Distance      Popup
+    Spots         Popup (could be single "2 spots")
+    Activities    Popup (can't be singles == catnav)
+    Amenities     Singles
+    Level         Singles
+    General       Singles
+    */
 
-    const pillStyle: React.CSSProperties = {
-        border: "1px solid #d6d6d6",
-        borderRadius: 32,
-        height: 24,
-        display: "inline-flex",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: "0 8px",
+    let timeLabel = `${props.timeRange[0]} – ${props.timeRange[1]}`
+    let filters = [timeLabel, "credits", "actvities", "amenities", "favorited"];
+    let pills = filters.map(name =>
+      <Frame style={{
+        fontFamily: "TT Norms",
         fontSize: 14,
         fontWeight: 500,
-        fontFamily: "TT Norms",
-    }
+        color: "#333",
+        position: "relative",
+        display: "inline-flex",
+        height: 28,
+        width: "auto",
+        padding: "4px 16px",
+        borderRadius: 100,
+        background: "#fff",
+        border: "1px solid #ddd",
+        marginRight: 4,
+      }} onTap={e => props.onCategoryChange(name)}>{name}</Frame>
+    )
+
 
     // const selectedStyle = Object.assign({}, tabStyle)
     // selectedStyle.color = "#05f"
 
+    // <Frame {...props} style={{background: "transparent"}}>
+    // <div style={pillStyle} >
+    //
+    // </div>
+    // </Frame>
     return (
-      <Frame {...props} style={{background: "transparent"}}>
-        <div style={pillStyle} >
-            {props.timeRange[0]} – {props.timeRange[1]}
-        </div>
-      </Frame>
+      <div>
+      <Scroll {...props} direction="horizontal" style={{
+          background: "#fff",
+      }}>
+        <Frame style={{
+          background: "transparent",
+          width: "auto",
+          whiteSpace: "nowrap",
+          paddingTop: 6,
+          paddingBottom: 6,
+          paddingLeft: 12,
+          paddingRight: 12,
+        }}>
+          {pills}
+        </Frame>
+      </Scroll>
+
+      </div>
     )
 }
-
+// <Frame
+// bottom={0}
+// left={0}
+// right={0}
+// style={{
+//   position: "fixed",
+//   zIndex: 10,
+//   height: "auto",
+//   background: "#fff",
+//   boxShadow: "0 0 20px rgba(0, 0, 0, .3)"
+// }}>
+//   Hello
+// </Frame>
 TimeFilter.defaultProps = {
-    height: 24,
+    height: 40,
     width: "100%",
 }
